411747804e636d2660e32ebea142ac26cc6e0cb4
Paige Watson <paige.watson@outlook.com>
2021-02-01 16:59:08 -0800

    attempting to check heroku connection

--- src/main/java/dev/paigewatson/layoutmaster/models/LayoutMasterApplication.java
+++ src/main/java/dev/paigewatson/layoutmaster/models/LayoutMasterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LayoutMasterApplication
 {
+    //start the application
     public static void main(String[] args)
     {
         SpringApplication.run(LayoutMasterApplication.class, args);
