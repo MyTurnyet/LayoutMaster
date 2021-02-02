fc38e45d03330cec36312dc652bcc48658e2c911
Paige Watson <paige.watson@outlook.com>
2021-02-02 13:18:47 -0800

    CarTypeService now lets the carTpe save to the repository

--- src/main/java/dev/paigewatson/layoutmaster/client/services/MongoCarTypeService.java
+++ src/main/java/dev/paigewatson/layoutmaster/client/services/MongoCarTypeService.java
@@ -7,6 +7,7 @@ import dev.paigewatson.layoutmaster.models.rollingstock.CarType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,14 +29,18 @@ public class MongoCarTypeService implements CarTypeService
     public List<CarType> allCarTypes()
     {
         final List<CarTypeDto> carTypeDtoList = carTypeRepository.findAll();
-        final List<CarType> carTypeList = (List<CarType>) carTypeDtoList.stream().map(carTypeDto -> carTypeDto.getEntity());
+        ArrayList<CarType> carTypeList = new ArrayList<>();
+        for (CarTypeDto carTypeDto : carTypeDtoList)
+        {
+            carTypeList.add(carTypeDto.getEntity());
+        }
         return carTypeList;
     }
 
     @Override
     public void saveCarTypeToDatabase(CarType carTypeToSave)
     {
-        carTypeRepository.save(carTypeToSave.getDto());
+        carTypeToSave.saveToRepository(carTypeRepository);
     }
 
 
--- src/test/java/dev/paigewatson/layoutmaster/client/services/CarTypeServiceTests.java
+++ src/test/java/dev/paigewatson/layoutmaster/client/services/CarTypeServiceTests.java
@@ -1,14 +1,22 @@
 package dev.paigewatson.layoutmaster.client.services;
 
+import dev.paigewatson.layoutmaster.data.models.CarTypeDto;
 import dev.paigewatson.layoutmaster.helpers.MongoCarTypeRepositoryFake;
+import dev.paigewatson.layoutmaster.models.goods.GoodsType;
 import dev.paigewatson.layoutmaster.models.rollingstock.AARDesignation;
+import dev.paigewatson.layoutmaster.models.rollingstock.CarType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static dev.paigewatson.layoutmaster.models.goods.GoodsType.Ingredients;
+import static dev.paigewatson.layoutmaster.models.goods.GoodsType.SheetMetal;
+import static dev.paigewatson.layoutmaster.models.rollingstock.AARDesignation.XM;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CarTypeServiceTests
@@ -37,38 +45,45 @@ public class CarTypeServiceTests
             //assert
             assertThat(aarDesignations.size()).isEqualTo(23);
         }
-//
-//        @Test
-//        public void should_returnListOfAllExistingCarTypesFromDatabase()
-//        {
-//            //assign
-//            //assign
-//            final ArrayList<GoodsType> carriedGoodsList = new ArrayList<>();
-//            carriedGoodsList.add(Ingredients);
-//
-//            final Dto<CarType> carTypeDto = new CarTypeDto("FOOO!", "XM", Arrays.asList(new String[]{"Stuff"}));
-//
-//           repositoryFake.setReturnedValuesList(returnedCarTypes);
-//
-//            //act
-//            List<CarType> allCarTypesList = service.allCarTypes();
-//            //assert
-//            assertThat(allCarTypesList).isEqualTo(returnedCarTypes);
-//        }
 
-//        @Test
-//        public void should_saveCarTypeToRepositoru()
-//        {
-//            //assign
-//            final ArrayList<GoodsType> carriedGoodsList = new ArrayList<>();
-//            carriedGoodsList.add(Ingredients);
-//
-//            final CarType boxCarType = new CarType(XM, carriedGoodsList);
-//
-//            //act
-//            service.saveCarTypeToDatabase(boxCarType);
-//            //assert
-//            assertThat(repositoryFake.savedEntity).isEqualTo(boxCarType);
-//        }
+
+        @Test
+        public void should_returnListOfAllExistingCarTypesFromDatabase()
+        {
+            //assign
+            //assign
+            final ArrayList<GoodsType> carriedGoodsList = new ArrayList<>();
+            carriedGoodsList.add(Ingredients);
+
+            final CarTypeDto carTypeDto = new CarTypeDto("FOOO!", "XM", Arrays.asList("SheetMetal"));
+            List<CarTypeDto> returnedCarTypes = Arrays.asList(carTypeDto);
+            repositoryFake.setReturnedValuesList(returnedCarTypes);
+
+            //act
+            List<CarType> allCarTypesList = service.allCarTypes();
+            //assert
+            assertThat(allCarTypesList.size()).isEqualTo(1);
+            assertThat(allCarTypesList.get(0).canCarry(SheetMetal)).isTrue();
+            assertThat(allCarTypesList.get(0).isOfType(XM)).isTrue();
+        }
+
+        @Test
+        public void should_saveCarTypeToRepository()
+        {
+            //assign
+            final ArrayList<GoodsType> carriedGoodsList = new ArrayList<>();
+            carriedGoodsList.add(Ingredients);
+
+            final CarTypeDto carTypeDto = new CarTypeDto("", "XM", Arrays.asList("Ingredients"));
+
+            final CarType boxCarType = new CarType(XM, carriedGoodsList);
+
+            //act
+            service.saveCarTypeToDatabase(boxCarType);
+            //assert
+            assertThat(repositoryFake.savedEntity.id).isEqualTo(carTypeDto.id);
+            assertThat(repositoryFake.savedEntity.aarType).isEqualTo(carTypeDto.aarType);
+            assertThat(repositoryFake.savedEntity.carriedGoods).isEqualTo(carTypeDto.carriedGoods);
+        }
     }
 }
