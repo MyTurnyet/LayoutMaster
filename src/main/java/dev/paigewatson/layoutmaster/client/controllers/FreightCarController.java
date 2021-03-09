496e5dc0462236aebb9698f7890125c33443d7b5
Paige Watson <paige.watson@outlook.com>
2021-03-09 15:08:13 -0800

    Add CrossOrigin annotation to controllers

--- src/main/java/dev/paigewatson/layoutmaster/client/controllers/FreightCarController.java
+++ src/main/java/dev/paigewatson/layoutmaster/client/controllers/FreightCarController.java
@@ -5,6 +5,7 @@ import dev.paigewatson.layoutmaster.models.rollingstock.AARDesignation;
 import dev.paigewatson.layoutmaster.models.rollingstock.FreightCar;
 import dev.paigewatson.layoutmaster.models.rollingstock.RollingStock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/inventory")
 public class FreightCarController
 {
